c33d9fa0e44526b12d54be71f0193de68c344779
Peter <peterng.918@gmail.com>
2023-06-08 11:49:26 -0400

    added scrolling to contact me

--- src/components/contactMe/ContactMe.js
+++ src/components/contactMe/ContactMe.js
@@ -20,131 +20,133 @@ const ContactMe = ({ darkMode }) => {
     console.log("body", body);
   }
   return (
-    <Box
-      width={{ xs: "100%", md: "100%" }}
-      py={"1rem"}
-      sx={
-        darkMode
-          ? {
-              alignItems: "center",
-              display: "flex",
-              flexDirection: "column",
-              backgroundColor: grey[800],
-            }
-          : {
-              alignItems: "center",
-              display: "flex",
-              flexDirection: "column",
-              backgroundColor: grey[200],
-            }
-      }
-    >
-      <h2 className={Style.pink}>Contact Me</h2>
+    <section id="contactMe">
+      <Box
+        width={{ xs: "100%", md: "100%" }}
+        py={"1rem"}
+        sx={
+          darkMode
+            ? {
+                alignItems: "center",
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: grey[800],
+              }
+            : {
+                alignItems: "center",
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: grey[200],
+              }
+        }
+      >
+        <h2 className={Style.pink}>Contact Me</h2>
 
-      <form onSubmit={submitHandler}>
-        <Stack spacing={2} direction="row" sx={{ mt: 4 }}>
+        <form onSubmit={submitHandler}>
+          <Stack spacing={2} direction="row" sx={{ mt: 4 }}>
+            <TextField
+              value={first}
+              variant="outlined"
+              onChange={(e) => setFirst(e.target.value)}
+              label="First Name"
+              required
+              fullWidth
+              // className={Style.contactDark}
+              sx={
+                darkMode
+                  ? {
+                      mb: 4,
+                      "& .MuiInputLabel-root": { color: "white" }, //styles the label
+                      "& .MuiOutlinedInput-root": {
+                        "& > fieldset": { borderColor: "white" },
+                      },
+                    }
+                  : { mb: 4 }
+              }
+              SelectProps={{
+                className: Style.contactDark,
+              }}
+            ></TextField>
+            <TextField
+              value={last}
+              onChange={(e) => setLast(e.target.value)}
+              label="Last Name"
+              required
+              fullWidth
+              sx={
+                darkMode
+                  ? {
+                      mb: 4,
+                      "& .MuiInputLabel-root": { color: "white" }, //styles the label
+                      "& .MuiOutlinedInput-root": {
+                        "& > fieldset": { borderColor: "white" },
+                      },
+                    }
+                  : { mb: 4 }
+              }
+              SelectProps={{
+                className: Style.contactDark,
+              }}
+            ></TextField>
+          </Stack>
           <TextField
-            value={first}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email"
+            required
+            fullWidth
+            sx={
+              darkMode
+                ? {
+                    mb: 4,
+                    "& .MuiInputLabel-root": { color: "white" }, //styles the label
+                    "& .MuiOutlinedInput-root": {
+                      "& > fieldset": { borderColor: "white" },
+                    },
+                  }
+                : { mb: 4 }
+            }
+            SelectProps={{
+              className: Style.contactDark,
+            }}
+          ></TextField>
+          <TextField
+            multiline
+            rows={8}
+            required
+            fullWidth
+            label="Body"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            sx={
+              darkMode
+                ? {
+                    mb: 4,
+                    "& .MuiInputLabel-root": { color: "white" }, //styles the label
+                    "& .MuiOutlinedInput-root": {
+                      "& > fieldset": { borderColor: "white" },
+                    },
+                  }
+                : { mb: 4 }
+            }
+            SelectProps={{
+              className: Style.contactDark,
+            }}
+          ></TextField>
+          <Button
+            type="submit"
             variant="outlined"
-            onChange={(e) => setFirst(e.target.value)}
-            label="First Name"
-            required
-            fullWidth
-            // className={Style.contactDark}
             sx={
               darkMode
-                ? {
-                    mb: 4,
-                    "& .MuiInputLabel-root": { color: "white" }, //styles the label
-                    "& .MuiOutlinedInput-root": {
-                      "& > fieldset": { borderColor: "white" },
-                    },
-                  }
-                : { mb: 4 }
+                ? { color: "white", borderColor: "white" }
+                : { color: "black", borderColor: "black" }
             }
-            SelectProps={{
-              className: Style.contactDark,
-            }}
-          ></TextField>
-          <TextField
-            value={last}
-            onChange={(e) => setLast(e.target.value)}
-            label="Last Name"
-            required
-            fullWidth
-            sx={
-              darkMode
-                ? {
-                    mb: 4,
-                    "& .MuiInputLabel-root": { color: "white" }, //styles the label
-                    "& .MuiOutlinedInput-root": {
-                      "& > fieldset": { borderColor: "white" },
-                    },
-                  }
-                : { mb: 4 }
-            }
-            SelectProps={{
-              className: Style.contactDark,
-            }}
-          ></TextField>
-        </Stack>
-        <TextField
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          label="Email"
-          required
-          fullWidth
-          sx={
-            darkMode
-              ? {
-                  mb: 4,
-                  "& .MuiInputLabel-root": { color: "white" }, //styles the label
-                  "& .MuiOutlinedInput-root": {
-                    "& > fieldset": { borderColor: "white" },
-                  },
-                }
-              : { mb: 4 }
-          }
-          SelectProps={{
-            className: Style.contactDark,
-          }}
-        ></TextField>
-        <TextField
-          multiline
-          rows={8}
-          required
-          fullWidth
-          label="Body"
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-          sx={
-            darkMode
-              ? {
-                  mb: 4,
-                  "& .MuiInputLabel-root": { color: "white" }, //styles the label
-                  "& .MuiOutlinedInput-root": {
-                    "& > fieldset": { borderColor: "white" },
-                  },
-                }
-              : { mb: 4 }
-          }
-          SelectProps={{
-            className: Style.contactDark,
-          }}
-        ></TextField>
-        <Button
-          type="submit"
-          variant="outlined"
-          sx={
-            darkMode
-              ? { color: "white", borderColor: "white" }
-              : { color: "black", borderColor: "black" }
-          }
-        >
-          Submit
-        </Button>
-      </form>
-    </Box>
+          >
+            Submit
+          </Button>
+        </form>
+      </Box>
+    </section>
   );
 };
 
